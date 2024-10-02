0458c9de257ca7bc95c503ea8e852000c0ecb14c
Sorod Yawa <sorod_yawa@cmu.ac.th>
2024-10-02 17:06:09 +0700

    4.6* Add next and prev button

--- src/main/java/se331/lab/Application.java
+++ src/main/java/se331/lab/Application.java
@@ -20,7 +20,8 @@ public class Application {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**")
-						.allowedOrigins("http://localhost:5173");
+						.allowedOrigins("http://localhost:5173")
+						.exposedHeaders("x-total-count");
 			}
 		};
 	}
